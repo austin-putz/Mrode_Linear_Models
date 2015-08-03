09383606b52d714d6561eb32990fa9f850f5a629
austin-putz <putz.austin@gmail.com>
2015-08-02 21:37:25 -0500

    added updated to the beginning of chapter 1

---
+++ chapter_1.R
@@ -0,0 +1,16 @@
+#==============================================================================#
+# Chapter 1: Genetic Evaluation with Different Sources of Records
+#==============================================================================#
+
+#------------------------------------------------------------------------------#
+# Description
+#------------------------------------------------------------------------------#
+
+# Raphael A. Mrode
+# Linear Models for the Prediction of Animal Breeding Values
+
+# Author:   Austin Putz <putz[dot]austin[at]gmail[dot]com>
+# Created:  Unknown
+# Modified: 2015-07-31
+# License:  GPLv2
+# Added a new line
